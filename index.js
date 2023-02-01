caa5140a41715dc67776a1431420bbd88f8f2404
jereconjota <jeremiasfigueroa69@gmail.com>
2023-02-01 10:06:27 -0300

    name, img and msg edited

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const { EmbedBuilder, WebhookClient } = require('discord.js');
 const webhookClient = new WebhookClient({ id: process.env.WEBHOOK_ID, token: process.env.WEBHOOK_TOKEN });
 
 const embed = new EmbedBuilder()
-	.setTitle('Dejanos unas fotos para alegrar la tarde 🐶🐱🐰🐴🦄🐦🐠')
+	.setTitle('🐶🐱🐰🐴🦄🐦🐠')
+	.setDescription('Dejanos unas fotos para alegrar la tarde')
 	.setColor(0x00FFFF);
 
 webhookClient.send({
-	content: 'Que estan haciendo tus mascotas?',
-	username: 'Rami',
-	avatarURL: 'https://jereconjota.com/_next/image?url=%2Fimages%2Fprofile.jpg&w=256&q=75',
-	embeds: [embed],
+	content: 'Que estan haciendo tus mascotas?\n🐶🐱🐰🐴🦄🐦🐠\nDejanos unas fotos para alegrar la tarde👇 ',
+	username: 'Genoshi',
+	avatarURL: 'https://i.postimg.cc/3R4jbs3t/IMG-0834.jpg',
+	// embeds: [embed],
 });
