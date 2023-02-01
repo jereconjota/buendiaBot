9d091a8abe2f5556bfbe042d09779d3fc7bea9b4
jereconjota <jeremiasfigueroa69@gmail.com>
2023-02-01 12:57:50 -0300

    cron job added & env variables

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Bot de discord (webhooks)
-### con comandos para pedir fotos de gatos y perros
+# Bot de discord (webhook)
+### Servidor node para enviar mensajes a discord usando webhooks
+
+---
+
+## Instalación
+
+```bash
+npm install
+```
+
+## Variables de entorno
+
+```env
+WEBHOOK_ID=123456789012345678
+WEBHOOK_TOKEN=your-token-goes-here
+
+PAYLOAD_CONTENT="Este es el cuerpo del mensaje"
+PAYLOAD_USERNAME=NombreDelBot
+PAYLOAD_AVATAR_URL=https://i.postimg.cc/3R4jbs3t/IMG-0834.jpg
+
+```
+
+## Uso
+
+```bash
+node index.js
+```
+
+---
+
+## Funcionamiento
+
+El bot se encarga de enviar mensajes a un canal de discord usando webhooks. Para ello se debe crear un webhook en el servidor de discord que administramos, especificar el canal a cual se enviara el mensaje y luego copiar el link para obtener el id y el token del webhook. Estos datos se deben agregar como variables de entorno en el archivo .env.
+
+El bot se encarga de enviar un mensaje con el contenido especificado en la variable PAYLOAD_CONTENT, el nombre del bot se especifica en la variable PAYLOAD_USERNAME y la imagen del bot se especifica en la variable PAYLOAD_AVATAR_URL.
+
+Especificar en la linea 48 del archivo index.js el intervalo de tiempo en el cual se enviara el mensaje. Por ejemplo:
+
+```js
+const job = nodeCron.schedule('0 30 11 18 *', sendToDiscord);
+```
+
+En este ejemplo el mensaje se enviara todos los dias a las 11:30 am del 18 de cada mes.
--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 require('dotenv').config()
+const ora = require('ora');
+const chalk = require('chalk');
+const nodeCron = require('node-cron');
 const { EmbedBuilder, WebhookClient } = require('discord.js');
 const webhookClient = new WebhookClient({ id: process.env.WEBHOOK_ID, token: process.env.WEBHOOK_TOKEN });
 
+// we can add embeds to the webhook
 const embed = new EmbedBuilder()
 	.setTitle('🐶🐱🐰🐴🦄🐦🐠')
 	.setDescription('Dejanos unas fotos para alegrar la tarde')
 	.setColor(0x00FFFF);
 
-webhookClient.send({
-	content: 'Que estan haciendo tus mascotas?\n🐶🐱🐰🐴🦄🐦🐠\nDejanos unas fotos para alegrar la tarde👇 ',
-	username: 'Genoshi',
-	avatarURL: 'https://i.postimg.cc/3R4jbs3t/IMG-0834.jpg',
+const messagePeyload = {
+	content: process.env.PAYLOAD_CONTENT,
+	username: process.env.PAYLOAD_USERNAME,
+	avatarURL: process.env.PAYLOAD_AVATAR_URL,
 	// embeds: [embed],
-});
+};
+
+function sendToDiscord() {
+
+	console.log(chalk.green("Running scheduled job"));
+	const spinner = ora({
+		text: "Sending message to Discord",
+		color: "blue",
+		hideCursor: false,
+	}).start();
+
+	try {
+		webhookClient.send(messagePeyload);
+		spinner.succeed("Message sent to Discord");
+		spinner.clear();
+		console.log(chalk.green("Finished at: " + new Date().toISOString()));
+		
+	} catch (error) {
+		spinner.fail("Error sending message to Discord");
+		spinner.clear();
+		console.log(chalk.red("Error: " + error));
+		
+	}
+}
+
+// job to run every 5 seconds
+// const job = nodeCron.schedule('*/5 * * * * *', sendToDiscord);
+
+// job to run every 1 month, on the 18th at 11:30:00
+const job = nodeCron.schedule('0 30 11 18 *', sendToDiscord);
+
+
+
+
